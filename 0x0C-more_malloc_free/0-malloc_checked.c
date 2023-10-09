13e1a3b463848901a51a52e16523cbe9b32b4815
benbihi <elm.benbihi@gmail.com>
2023-10-08 18:09:54 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+* malloc_checked - Returns a pointer to the allocated memory
+* @b: number of bytes to allocate
+* Return: pointer to the allocated memory`
+*/
+void *malloc_checked(unsigned int b)
+{
+/* let's first create the pointer */
+void *ptr;
+/* now we try to allocate the required number of bytes */
+ptr = malloc(b);
+/* and we check that malloc succeeded. If notm exit with code 98 */
+if (ptr == NULL)
+exit(98);
+/* everything worked fine */
+return (ptr);
+}
