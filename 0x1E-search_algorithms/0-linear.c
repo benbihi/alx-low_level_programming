d3e0928d8c6672d61c278e3f0d092362f2a314ac
benbihi <elm.benbihi@gmail.com>
2024-07-17 12:27:29 -0700

    task 0 2

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,14 +11,13 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%li] = [%i]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			printf("Found %i at index: %li", value, i);
 			return (i);
 		}
 	}
